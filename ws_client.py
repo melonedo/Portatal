b1f35703db073383a06b63b6eddd0f9f14865ad8
melonedo <44501064+melonedo@users.noreply.github.com>
2020-10-15 15:14:43 +0800

    为查电费搭建一个奇妙的VPN
    我为什么不直接搭VPN呢???

---
+++ ws_client.py
@@ -0,0 +1,26 @@
+# 树莓派主动联系服务器以提供查询电费服务
+
+import asyncio
+import aiohttp
+from electricity import query_electricity
+
+url = 'http://172.81.215.215/piconnect'
+
+async def main():
+    async with aiohttp.ClientSession() as s:
+        while True:
+            try:
+                async with s.ws_connect(url) as ws:
+                    while True:
+                        print("Websocket connected.")
+                        await ws.receive_str()
+            except KeyboardInterrupt:
+                break
+            print("Websocket disconnected, retry in 5 mins.")
+            await asyncio.sleep(300)
+        
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
---
+++ ws_server.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.responses import RedirectResponse
+import asyncio
+
+app = FastAPI()
+
+ip = None
+
+@app.get("/pi/{more_path:path}")
+async def forward(req: Request, more_path: str):
+    return RedirectResponse(ip + more_path, 307, req.headers)
+
+@app.websocket("/piconnect")
+async def elec_ws(ws: WebSocket):
+    global ip
+    if ip:
+        await ws.close()
+        return
+    await ws.accept()
+    ip = f"http://{ws.client.host}:1234/"
+    
+    print(f"{ip} connected")
+    try:
+        while True:
+            # Not much to do
+            await ws.receive_text()
+            
+    except WebSocketDisconnect:
+        print(f"{ip} diconnected")
+        ip = None
+        await ws.close()
