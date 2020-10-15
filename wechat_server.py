f6d18f54cc7e97ea64b94979ad656ef8d9461769
melonedo <44501064+melonedo@users.noreply.github.com>
2020-10-15 17:39:21 +0800

    把WebSocket扔到主文件里

--- wechat_server.py
+++ wechat_server.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocketDisconnect, WebSocket
 from hashlib import sha1
 import json
 from fastapi.responses import PlainTextResponse
@@ -8,6 +8,8 @@ app = FastAPI()
 
 with open("data/secrets.json") as f:
     secrets = json.load(f)
+
+ip = None
 
 @app.get("/wechat")
 async def verify_wechat(signature: str, timestamp: str, nonce: str, echostr: str):
@@ -21,6 +23,22 @@ async def verify_wechat(signature: str, timestamp: str, nonce: str, echostr: str
         print("认证失败")
         return PlainTextResponse("")
 
+@app.websocket("/piconnect")
+async def websocket_endpoint(ws: WebSocket):
+    global ip
+    if ip:
+        await ws.close()
+        return
+    await ws.accept()
+    ip = f"http://{ws.client.host}:4321/"
 
+    print(f"{ip} connected")
+    try:
+        while True:
+            # Not much to do
+            await ws.receive_text()
 
-
+    except WebSocketDisconnect:
+        print(f"{ip} disconnected")
+        ip = None
+        await ws.close()
